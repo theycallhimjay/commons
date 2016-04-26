2948de08e8f1009f97cb75f583b4a28ec39ac1f9
theycallhimjay <demeterjason@gmail.com>
2016-04-26 08:35:09 -0500

    HelloWorld C# app

---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal();
+            Console.WriteLine(cow.CreateSound());
+        }
+
+        public static int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public static decimal Add(decimal val1, decimal val2)
+        {
+            return val1 + val2;
+        }
+
+        public static String fizzBuzz(int value)
+        {
+            if (value % 15 == 0)
+                return "FizzBuzz";
+            else if (value % 3 == 0)
+                return "Fizz";
+            else if (value % 5 == 0)
+                return "Buzz";
+            else return value.ToString();
+        }
+
+    }
+
+    public class Test
+    {
+        public Test()
+        {
+
+        }
+
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+    }
+
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public virtual string CreateSound()
+        {
+            return "Bloop";
+        }
+    }
+
+
+    public class Cow : Animal
+    {
+        public Cow()
+        {
+
+        }
+
+        public override string CreateSound()
+        {
+            return "Moooo";
+        }
+    }
+}
